5c914ddd0e245ae4ac5525f6b8a4788fba0a58e5
komuw <komuw05@gmail.com>
2019-10-05 19:32:19 +0300

    m

--- main.go
+++ main.go
@@ -56,11 +56,17 @@ func main() {
 	}
 
 	fmt.Println("NextReviewAt() 1: ", card.Algorithm.NextReviewAt())
+
+	// TODO: this is where we rely on user input for them to rate this card
+	// then we call advance based on user input.
+	// Remember to validate the user input
 	// review and rate a card
 	sm := card.Algorithm.Advance(0.8)
 	card.Algorithm = sm
 	fmt.Println("NextReviewAt() 2: ", card.Algorithm.NextReviewAt())
 
+	// After the user has rated the card and we have updated the card struct with the new metadata
+	// We need to  persist that on the markdown files' extended attributes
 	// update the card attributes with new algo
 	algoJson, err := json.Marshal(card)
 	if err != nil {
@@ -74,6 +80,7 @@ func main() {
 	fmt.Println("card when saving")
 	litter.Dump(card)
 
+	// TODO: finally we display the answer to the user
 	err = card.Display()
 	if err != nil {
 		log.Fatalf("error: %+v", err)
