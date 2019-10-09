65a206d28dabd2c94e48434d7632ac5f2f8cb9ef
komuW <komuw05@gmail.com>
2019-10-09 16:25:42 +0300

    a

--- srs.go
+++ srs.go
@@ -2,6 +2,7 @@ package main
 
 import "time"
 
+// SRSalgorithm is the interface that all posible srs algo's should satisfy
 type SRSalgorithm interface {
 	NextReviewAt() time.Time
 	Advance(rating float64) SRSalgorithm
