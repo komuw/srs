802840f2414dd35e61dfdaaabb7a0224e6750f6f
komuW <komuw05@gmail.com>
2019-10-14 18:42:07 +0300

    f

--- card.go
+++ card.go
@@ -59,7 +59,7 @@ func NewCard(filepath string) (*Card, error) {
 		Version:   1,
 		Question:  question,
 		FilePath:  filepath,
-		Algorithm: NewEbisu(), //NewSupermemo2(),
+		Algorithm: NewSupermemo2(), //NewEbisu(),
 	}
 	if len(cardAttribute) > 0 {
 		// if cardAttribute exists, then this is not a new card and we should
