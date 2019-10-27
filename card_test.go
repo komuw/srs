eeb1de77df9c19665396423dc4eac1542aed31e2
komuw <komuw05@gmail.com>
2019-10-27 03:26:22 +0300

    m

--- card_test.go
+++ card_test.go
@@ -1,16 +1,45 @@
 package srs
 
 import (
+	"io/ioutil"
+	"log"
+	"os"
 	"testing"
 	"time"
 )
 
-//TODO: We should remove all file extended attributes before every test
+func tempfile() string {
+	f, err := ioutil.TempFile("", "srs-testDB-")
+	if err != nil {
+		panic(err)
+	}
+	if err := f.Close(); err != nil {
+		panic(err)
+	}
+	if err := os.Remove(f.Name()); err != nil {
+		panic(err)
+	}
+	return f.Name()
+}
+
+func dbRemoverUtil(path string) {
+	err := os.Remove(path)
+	if err != nil {
+		panic(err)
+	}
+}
 
 func TestCardCreation(t *testing.T) {
+	dbPath := tempfile()
+	defer dbRemoverUtil(dbPath)
+	db, err := OpenDb(dbPath)
+	if err != nil {
+		log.Fatal("unable to open test db at:: ", dbPath, err)
+	}
+	defer db.Close()
 
 	cardPath := "./testdata/test1.md"
-	_, err := NewCard(cardPath)
+	_, err = NewCard(cardPath, db)
 	if err != nil {
 		t.Errorf("\nCalled NewCard(%#+v) \ngot %s \nwanted %#+v", cardPath, err, nil)
 	}
@@ -18,8 +47,16 @@ func TestCardCreation(t *testing.T) {
 }
 
 func TestCardReviewAt(t *testing.T) {
+	dbPath := tempfile()
+	defer dbRemoverUtil(dbPath)
+	db, err := OpenDb(dbPath)
+	if err != nil {
+		log.Fatal("unable to open test db at:: ", dbPath, err)
+	}
+	defer db.Close()
+
 	cardPath := "./testdata/test1.md"
-	card, err := NewCard(cardPath)
+	card, err := NewCard(cardPath, db)
 	if err != nil {
 		t.Errorf("\nCalled NewCard(%#+v) \ngot %#+v \nwanted %#+v", cardPath, err, nil)
 	}
@@ -34,12 +71,19 @@ func TestCardReviewAt(t *testing.T) {
 }
 
 func TestCardAdvance(t *testing.T) {
+	dbPath := tempfile()
+	defer dbRemoverUtil(dbPath)
+	db, err := OpenDb(dbPath)
+	if err != nil {
+		log.Fatal("unable to open test db at:: ", dbPath, err)
+	}
+	defer db.Close()
 
 	t.Run("rating less than ratingSuccess", func(t *testing.T) {
 		// rating less than `ratingSuccess`
 		rating := 0.4
 		cardPath := "./testdata/test1.md"
-		card, err := NewCard(cardPath)
+		card, err := NewCard(cardPath, db)
 		if err != nil {
 			t.Errorf("\nCalled NewCard(%#+v) \ngot %#+v \nwanted %#+v", cardPath, err, nil)
 		}
@@ -57,7 +101,7 @@ func TestCardAdvance(t *testing.T) {
 		// rating greater than `ratingSuccess`
 		rating := 0.9
 		cardPath := "./testdata/test1.md"
-		card, err := NewCard(cardPath)
+		card, err := NewCard(cardPath, db)
 		if err != nil {
 			t.Errorf("\nCalled NewCard(%#+v) \ngot %#+v \nwanted %#+v", cardPath, err, nil)
 		}
