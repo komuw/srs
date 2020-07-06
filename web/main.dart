31bf382e38fd008c1e8117a56d8236a5a3713552
komuW <komuw05@gmail.com>
2020-07-06 11:39:07 +0300

    f

---
+++ web/main.dart
@@ -0,0 +1,15 @@
+import 'dart:html';
+
+/*
+webdev serve \
+        --debug \
+        --debug-extension \
+        --injected-client \
+        --log-requests \
+        --output=web:output \
+        web:8080
+# --release
+*/
+void main() {
+  querySelector("#RipVanWinkle")?.text = "Wake up, sleepy head!";
+}
