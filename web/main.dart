fc9252cc35d474afe237209896959124fe8def77
komuW <komuw05@gmail.com>
2020-07-06 16:31:02 +0300

    f

--- web/main.dart
+++ web/main.dart
@@ -11,11 +11,37 @@ webdev serve \
         web:8080
 # --release
 */
-void main() {
-  html.DivElement div = html.querySelector("#app") as html.DivElement;
-  print("p.type: ${div.runtimeType}");
-  div.text = "New app text, via Dartlang!";
 
+late html.DivElement MainDiv;
+late html.InputElement TagNameInput;
+late html.InputElement TagDescriptionInput;
+
+void main() {
   var defaultTags = srs.generateDefaultTags();
   print("defaultTags: ${defaultTags}");
+  MainDiv = html.querySelector("#app") as html.DivElement;
+
+  // html.ButtonElement AddTagsButton =
+  //     html.querySelector("#buttonAddTags") as html.ButtonElement;
+  // print("AddTagsButton.type: ${AddTagsButton.runtimeType}");
+  // AddTagsButton.onClick.listen(addTags);
+
+  TagNameInput = html.querySelector("#tagName") as html.InputElement;
+  TagDescriptionInput =
+      html.querySelector("#tagDescription") as html.InputElement;
+
+  TagDescriptionInput.onChange.listen(addTags);
+}
+
+void addTags(html.Event e) {
+  print("addTags func called.");
+  print("with event: $e");
+
+  var tName = TagNameInput.value;
+  var tDescription = TagDescriptionInput.value;
+
+  print("tName: $tName , tDescription: $tDescription");
+  var t = srs.Tag(tName, tDescription);
+
+  print("new Tag created: $t");
 }
